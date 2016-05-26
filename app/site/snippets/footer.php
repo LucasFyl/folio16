15a7d03b05e0e2385663eed048e8772932b395ff
Lucas Fayolle <lfayolle1@gmail.com>
2016-05-26 21:25:54 +0200

    include analytics

--- app/site/snippets/footer.php
+++ app/site/snippets/footer.php
@@ -7,6 +7,18 @@
 		<div></div>
 		<div></div>
 	</div>
+
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-40894794-1']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
 	
 	<?= js('/bower_components/jquery/dist/jquery.js') ?>
 	<?= js('/bower_components/gsap/src/minified/TweenMax.min.js') ?>
